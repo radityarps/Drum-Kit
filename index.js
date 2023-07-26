81577dbb87fcb7e26badad50376e77d1703c64e4
radityarps <radityarafifps@gmail.com>
2023-07-26 17:02:29 +0700

    created audio system
    1. Link js file to html
    2. added background image to each button
    3. created event listener to a button to play the sound when clicked

---
+++ index.js
@@ -0,0 +1,40 @@
+// Drum Button
+var drumButton = document.querySelectorAll(".drum");
+for (var i = 0; i < drumButton.length; i++) {
+  drumButton[i].addEventListener("click", function () {
+    var whatButton = this.innerHTML;
+
+    switch (whatButton) {
+      case "w":
+        const tom1Audio = new Audio("./sounds/tom-1.mp3");
+        tom1Audio.play();
+        break;
+      case "a":
+        const tom3Audio = new Audio("./sounds/tom-3.mp3");
+        tom3Audio.play();
+        break;
+      case "s":
+        const tom4Audio = new Audio("./sounds/tom-4.mp3");
+        tom4Audio.play();
+        break;
+      case "d":
+        const tom2Audio = new Audio("./sounds/tom-2.mp3");
+        tom2Audio.play();
+        break;
+      case "j":
+        const snareAudio = new Audio("./sounds/snare.mp3");
+        snareAudio.play();
+        break;
+      case "k":
+        const crashAudio = new Audio("./sounds/crash.mp3");
+        crashAudio.play();
+        break;
+      case "l":
+        const kickBassAudio = new Audio("./sounds/kick-bass.mp3");
+        kickBassAudio.play();
+        break;
+      default:
+        break;
+    }
+  });
+}
